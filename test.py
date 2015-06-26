8a9ee25e8eabad573328598525b224aacef2fe1a
WindFlower <WindFlower@WindFlowers-MacBook-Air.local>
2015-06-26 10:26:28 -0500

    Add lines

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 x = 3
 y = 5
 print('The sum of ', x, ' plus ', y, ' is ', x + y)
+
+number = int(input("Enter an Integer: "))
+if number < 100:
+    print("Your number is smller than 100")
+else:
+    print("Your number is greater than 100")
+
